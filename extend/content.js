f570bd733e6ebf2234334ddcb398bdfed586efc1
fxl0206 <fxl0206@gmail.com>
2014-10-25 12:58:02 +0800

    format

--- extend/content.js
+++ extend/content.js
@@ -1,27 +1,27 @@
-$(function(){
-	alert("hre");
-	
-	chrome.extension.sendRequest({greeting: "hello"}, function(response) {
-  console.log(response.farewell);
-});
+$(function () {
+    alert("hre");
+
+    chrome.extension.sendRequest({greeting: "hello"}, function (response) {
+        console.log(response.farewell);
+    });
 
 
-chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
+    chrome.extension.onRequest.addListener(
+        function (request, sender, sendResponse) {
+            console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-                
-                	chrome.extension.sendRequest({greeting: "hello"}, function(response) {
-  console.log(response.farewell);
+
+            chrome.extension.sendRequest({greeting: "hello"}, function (response) {
+                console.log(response.farewell);
+            });
+
+
+            if (request.greeting == "hello")
+                sendResponse({farewell: "goodbye"});
+            else
+                sendResponse({farewell: "test"}); // snub them.
+        });
+
+
 });
-                
-                
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-    else
-      sendResponse({farewell:"test"}); // snub them.
-  });
-
-
-	});
