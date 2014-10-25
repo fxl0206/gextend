e2b9e31835880c4803101f05f72e35defcde28eb
fxl0206 <fxl0206@gmail.com>
2014-10-25 20:59:41 +0800

    format

--- extend/lib/options.js
+++ extend/lib/options.js
@@ -1,6 +1,6 @@
 ﻿$(function(){
     var defaults=new Array();
-    defaults["lastName"]="文昊";
+    defaults["lastName"]="欣欣";
     defaults["firstName"]="张";
     defaults["em"]="fxl0206@qq.com";
     defaults["pwd"]="fxL286955";
@@ -35,23 +35,6 @@
     $("#cancelStore").click(function(){
          window.close();
     });
-  /* $("#lastName").val("文昊");
-   $("#firstName").val("张");
-   $("#em").val("fxl0206@qq.com");
-   $("#pwd").val("fxL286955");
-   $("#c-pwd").val("fxL286955");
-   $("#sec-q-1").val("139");
-   $("#sec-q-2").val("143");
-   $("#sec-q-3").val("144");
-   $("#sec-ans-1").val("千里千寻");
-   $("#sec-ans-2").val("宫崎骏");
-   $("#sec-ans-3").val("火星人");
-   $("#dob-year").val("2000");
-   $("#dob-mon").val("02");
-   $("#dob-day").val("6");
-   $("#addr-l-1").val("浙江杭州");
-   $("#to-ci").val("杭州西湖区");
-    alert(localStorage["lastName"]);*/
    /* $.ajax({
         type:"post",
         dataType:"text",
